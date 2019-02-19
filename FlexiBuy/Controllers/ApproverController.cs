b8e00eb1e11aa94aa531960a34d2d80baa4f7b18
Anoop Simon <anoop.simon@adudev.com>
2019-02-20 09:52:15 +1100

    Added approver details

---
+++ FlexiBuy/Controllers/ApproverController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FlexiBuy.Models;
+using FlexiBuy.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlexiBuy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApproverController : ControllerBase
+    {
+       
+        // GET api/Approve/5
+        [HttpGet("{id}")]
+        public ActionResult<ApproverDetails> GetApproverDetail(int approverId)
+        {
+            return new ApproverService().GetApproverDetails(approverId);
+        }
+
+    }
+}
---
+++ FlexiBuy/Services/ApproverService.cs
@@ -0,0 +1,30 @@
+﻿using FlexiBuy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlexiBuy.Services
+{
+    public class ApproverService
+    {
+        public string GetApproverName(int approverId)
+        {
+            return "John Snow";
+        }
+
+        public ApproverDetails GetApproverDetails(int approverId)
+        {
+            return new ApproverDetails
+            {
+                ApproverId= 102,
+                Department = "Sales",
+                Email = "approver1@flexibuy.com",
+                FirstName = "John",
+                LastName = "Snow",
+                Role= "Manager"
+
+            };
+        }
+    }
+}
